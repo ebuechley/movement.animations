bf2eb58239eff3efd23f10fd0cff26aac2119afc
Evan <ebuechley@gmail.com>
2019-08-17 10:50:50 -0600

    all migrations one cycle

--- EV.moveVis.R
+++ EV.moveVis.R
@@ -9,6 +9,7 @@ library(moveVis)
 library(move)
 library(lubridate)
 library(plyr)
+library(dplyr)
 
 #load data
 #d = read.csv("ev.tv.filtered.csv")
@@ -51,23 +52,38 @@ d$timestamp <- ymd_hms(d$date, tz='GMT')
 #d$timestamp <- ymd_hms(d$DateTime, tz='GMT')
 class(d$timestamp)
 summary(d$timestamp)
+d$year = year(d$timestamp)
+d$month = month(d$timestamp)
+d$day = day(d$timestamp)
+d$hour = hour(d$timestamp)
+summary(d)
+d$year.null = 2000
+d$ymd = paste(d$year.null, d$month, sep = "-")
+d$ymd = paste(d$ymd, d$day, sep = "-")
+d$ymd = paste(d$ymd, d$hour, sep = " ")
+d$ymd = ymd_h(d$ymd)
+head(d)  
+class(d$ymd)
+
+# remove id.yr with few points
+summary(d$id.yr)
+d1 = d %>% group_by(id.yr) %>% filter(n()>=10) %>% ungroup()
 
 #
 #unique(d$id)
 #ev = subset(d, species == "Neophron percnopterus")
 #Logiya = subset(d, id == "Logiya")
-ev = d
+ev = d1
 summary(ev)
 names(ev)
 
 # use df2move to convert the data.frame into a moveStack
 dm = df2move(ev, proj = "+proj=longlat +datum=WGS84",
-             x = 'long', y = 'lat', time = 'timestamp', 
-             track_id = 'id')
-dm
+             x = 'long', y = 'lat', time = 'ymd', 
+             track_id = 'id.yr')
 
 # align move_data to a uniform time scale
-move_data <- align_move(dm, res = 3, digit = 0, unit = "days")
+move_data <- align_move(dm, res = 8, digit = 0, unit = "hours")
 #move_data <-  align_move(dm, res = "mean")
 
 # create spatial frames 
@@ -76,31 +92,33 @@ get_maptypes()
 #To use mapbox maps, you need to register for a free mapbox account and get a token key, which can be inserted below
 frames <- frames_spatial(move_data, alpha = 1, map_res = 1, margin_factor = 1.2,
                          #map_service = "osm", map_type = "no_labels",
-                         #map_service = "mapbox", map_type = "satellite", map_token = "pk.eyJ1IjoiZWJ1ZWNobGV5IiwiYSI6ImNqc2xiZXYxejBxanA0NHBpOWhndnRzbDMifQ.JKpJkhVzqWqJbgjNZzLKnA",
+                         map_service = "mapbox", map_type = "satellite", map_token = "pk.eyJ1IjoiZWJ1ZWNobGV5IiwiYSI6ImNqc2xiZXYxejBxanA0NHBpOWhndnRzbDMifQ.JKpJkhVzqWqJbgjNZzLKnA",
                          #map_service = "osm", map_type = "terrain",
-                         #map_service = "mapbox", map_type = "terrain", map_token = "pk.eyJ1IjoiZWJ1ZWNobGV5IiwiYSI6ImNqc2xiZXYxejBxanA0NHBpOWhndnRzbDMifQ.JKpJkhVzqWqJbgjNZzLKnA",
-                         map_service = "carto", map_type = "light_no_labels",
+                         #map_service = "carto", map_type = "light_no_labels",
+                         #map_service = "osm", map_type = "terrain",
                          map_dir = "~/Documents/MapDirectory/",
                          #ext = extent, 
                          equidistant = F,
                          path_size = 1, path_end = "round", path_join = "round", path_fade = T, 
                          #path_colours = c('red', 'green', '#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999','#000120'),
                          path_colours = NA,
-                         tail_length = 50, tail_size = .2, tail_colour = 'black', trace_show = T, trace_colour = 'black', 
+                         tail_length = 50, tail_size = .2, tail_colour = '#ff7f00', trace_show = T, trace_colour = '#ff7f00', 
                          path_legend = FALSE)
 length(frames)
 
 #customize frames
-frames <- add_labels(frames, title = "Egyptian Vulture Migration", 
-                     subtitle = "W.L. Phipps, P. López-López, E. Buechley, S. Oppel, et al. (2019)") 
-#frames <- add_scalebar(frames, height = 0.02, distance = 2000, x = -20, y = -1) # add a scale bar
+frames <- add_labels(frames, title = "Egyptian Vulture Migrations", 
+caption = "W.L. Phipps, P. López-López, E. Buechley, S. Oppel, et al. (2019)
+Spatial and Temporal Variability in Migration of a Soaring Raptor Across Three Continents
+Frontiers in Ecology and Evolution") 
+frames <- add_scalebar(frames, height = 0.01, distance = 1500, x = -20, y = 0, label_margin = 2, colour = "white") # add a scale bar
 #frames <- add_northarrow(frames, x = 50, y = -1) # add a north arrow
-frames <- add_timestamps(frames, move_data, type = "label") # add timestamps
+#frames <- add_timestamps(frames, move_data, type = "label") # add timestamps
 frames <- add_labels(frames, x = "Longitude", y = "Latitude") 
 frames <- add_progress(frames, size = 2) # add a progress bar
-frames[[1300]]
+frames[[1000]]
 
 # animate frame
 suggest_formats()
-animate_frames(frames, out_file = "./Outputs/EgyptianVulture_MovementAnimation_MigrationFlexibility_completmigrationsonly3.mp4", overwrite = TRUE,
-               fps = 10, end_pause = 5, res = 1000, width = 6500, height = 5000,)
+animate_frames(frames, out_file = "./Outputs/EgyptianVulture_MovementAnimation_MigrationFlexibility_Satellite.mp4", overwrite = TRUE,
+               fps = 10, end_pause = 5, res = 1000, width = 9000, height = 7000,)
