229bd46d0eb534399922b97f7676fa8fec446bd2
Evan <ebuechley@gmail.com>
2019-08-17 20:15:42 -0600

    final EV migr. flex. - combined migrations

--- EV.moveVis.R
+++ EV.moveVis.R
@@ -10,15 +10,22 @@ library(move)
 library(lubridate)
 library(plyr)
 library(dplyr)
+library(ggplot2)
+require(maps)
 
 #load data
 #d = read.csv("ev.tv.filtered.csv")
 #d = read.csv("EV.migration.flexibility.csv")
 #d = read.csv("EV.migration.flexibility.completmigrationsonly.csv")
-#d = read.csv("EV-CompletedMigrations-1ptperday.csv")
+d = read.csv("EV-CompletedMigrations-1ptperday.csv")
 #d = read.csv("EGVU_Final_complete_migrations_only_1ptperday.csv")
-d = read.csv("EV-all-1ptperday-filtered-utm-NSD-season.csv")
+#d = read.csv("EV-all-1ptperday-filtered-utm-NSD-season.csv")
 head(d)
+
+#quick plot of dataset
+map.plot = ggplot() + annotation_map(map_data("world"), fill = 'grey', color = "white") + coord_quickmap() + theme_bw() 
+map.plot = map.plot + geom_path(data = d, aes(long,lat, group = id), alpha = .5) 
+map.plot
 
 #add population variable
 unique(d$study)
@@ -48,8 +55,9 @@ d$colour[d$population == 'balkans'] <- '#4daf4a'
 d$colour[d$population == 'middle east'] <- '#e41a1c'
 
 # Re-store DateTime_GMT as POSIXt object
-d$timestamp <- ymd_hms(d$date, tz='GMT')
-#d$timestamp <- ymd_hms(d$DateTime, tz='GMT')
+head(d)
+#d$timestamp <- ymd_hms(d$date, tz='GMT')
+d$timestamp <- mdy_hm(d$date, tz='GMT')
 class(d$timestamp)
 summary(d$timestamp)
 d$year = year(d$timestamp)
@@ -67,13 +75,13 @@ class(d$ymd)
 
 # remove id.yr with few points
 summary(d$id.yr)
-d1 = d %>% group_by(id.yr) %>% filter(n()>=10) %>% ungroup()
+d1 = d %>% group_by(id.yr) %>% filter(n()>=100) %>% ungroup()
 
 #
 #unique(d$id)
 #ev = subset(d, species == "Neophron percnopterus")
 #Logiya = subset(d, id == "Logiya")
-ev = d1
+ev = d
 summary(ev)
 names(ev)
 
@@ -83,7 +91,7 @@ dm = df2move(ev, proj = "+proj=longlat +datum=WGS84",
              track_id = 'id.yr')
 
 # align move_data to a uniform time scale
-move_data <- align_move(dm, res = 8, digit = 0, unit = "hours")
+move_data <- align_move(dm, res = 1, digit = 0, unit = "days")
 #move_data <-  align_move(dm, res = "mean")
 
 # create spatial frames 
@@ -96,29 +104,29 @@ frames <- frames_spatial(move_data, alpha = 1, map_res = 1, margin_factor = 1.2,
                          #map_service = "osm", map_type = "terrain",
                          #map_service = "carto", map_type = "light_no_labels",
                          #map_service = "osm", map_type = "terrain",
-                         map_dir = "~/Documents/MapDirectory/",
+                         #map_dir = "~/Documents/MapDirectory/",
                          #ext = extent, 
                          equidistant = F,
                          path_size = 1, path_end = "round", path_join = "round", path_fade = T, 
                          #path_colours = c('red', 'green', '#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999','#000120'),
                          path_colours = NA,
-                         tail_length = 50, tail_size = .2, tail_colour = '#ff7f00', trace_show = T, trace_colour = '#ff7f00', 
+                         tail_length = 5, tail_size = .1, tail_colour = '#ff7f00', trace_show = T, trace_colour = '#ff7f00', 
                          path_legend = FALSE)
 length(frames)
 
 #customize frames
-frames <- add_labels(frames, title = "Egyptian Vulture Migrations", 
+frames <- add_labels(frames, title = "Egyptian Vulture Migration", 
 caption = "W.L. Phipps, P. López-López, E. Buechley, S. Oppel, et al. (2019)
 Spatial and Temporal Variability in Migration of a Soaring Raptor Across Three Continents
 Frontiers in Ecology and Evolution") 
-frames <- add_scalebar(frames, height = 0.01, distance = 1500, x = -20, y = 0, label_margin = 2, colour = "white") # add a scale bar
+frames <- add_scalebar(frames, height = 0.01, distance = 1500, x = -20, y = 0, label_margin = 2, colour = "black") # add a scale bar
 #frames <- add_northarrow(frames, x = 50, y = -1) # add a north arrow
 #frames <- add_timestamps(frames, move_data, type = "label") # add timestamps
 frames <- add_labels(frames, x = "Longitude", y = "Latitude") 
 frames <- add_progress(frames, size = 2) # add a progress bar
-frames[[1000]]
+frames[[300]]
 
 # animate frame
 suggest_formats()
-animate_frames(frames, out_file = "./Outputs/EgyptianVulture_MovementAnimation_MigrationFlexibility_Satellite.mp4", overwrite = TRUE,
-               fps = 10, end_pause = 5, res = 1000, width = 9000, height = 7000,)
+animate_frames(frames, out_file = "./Outputs/EgyptianVulture_MovementAnimation_MigrationFlexibility_Clean.mp4", overwrite = TRUE,
+               fps = 5, end_pause = 3, res = 1000, width = 10000, height = 8000,)
